2f392c187c97741fa0b8f9a88f7e6359ec7d14ff
Shahnawaz Haque <shahnawaz@Shahnawazs-MacBook.local>
2018-05-23 00:27:52 +0530

    Working copy of Loc8r app till chapter 6 of mean book

---
+++ app_api/models/db.js
@@ -0,0 +1,44 @@
+var mongoose = require('mongoose');
+//var dbURI = 'mongodb://localhost/loc8r';
+var dbURI = 'mongodb://admin:admin@ds023475.mlab.com:23475/loc8r-dev';
+if (process.env.NODE_ENV === 'production') {
+  dbURI = 'mongodb://admin:admin@ds023475.mlab.com:23475/loc8r-dev';
+}
+
+require('./locations');
+mongoose.connect(dbURI);
+mongoose.connection.on('connected',function (){
+  console.log('Mongoose connected to ' + dbURI);
+});
+mongoose.connection.on('error',function (){
+  console.log('Mongoose recevied error to ' + dbURI);
+});
+mongoose.connection.on('disconnected',function (){
+  console.log('Mongoose connection terminated ' + dbURI);
+});
+
+var gracefulShutdown = function (msg,callback){
+  mongoose.connection.closed(function (){
+    console.log('Mongoose connection disconnected through' + msg);
+    callback();
+  })
+}
+
+// For nodemon restarts
+process.once('SIGUSR2', function() {
+  gracefulShutdown('nodemon restart', function() {
+    process.kill(process.pid, 'SIGUSR2');
+  });
+});
+// For app termination
+process.on('SIGINT', function() {
+  gracefulShutdown('app termination', function() {
+    process.exit(0);
+  });
+});
+// For Heroku app termination
+process.on('SIGTERM', function() {
+  gracefulShutdown('Heroku app termination', function() {
+    process.exit(0);
+  });
+});
